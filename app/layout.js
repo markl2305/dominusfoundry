efac5f27591d2926dfa06eb790b64ad431410e5f
markl2305 <markflord@me.com>
2025-10-29 09:47:40 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,29 @@
 // app/layout.js
 import './globals.css'
 import Script from 'next/script'
+import GA4PageView from './ga-pageview'
+
+export const metadata = {
+  title: 'Dominus Foundry',
+  description: 'Systems, strategy, and smart rooms.',
+}
 
 export default function RootLayout({ children }) {
   const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className="min-h-screen bg-neutral-950 text-white">
+        {/* Page content */}
+        <div className="min-h-screen flex flex-col">{children}</div>
 
+        {/* SPA page_view tracking on route changes */}
+        <GA4PageView />
+
+        {/* GA4 base tag */}
         {GA_ID && (
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
               strategy="afterInteractive"
             />
-            <Script id="ga4" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                // we'll send page_view manually so SPA navigations are accurate
-                gtag('config', '${GA_ID}', { send_page_view: false });
-              `}
-            </Script>
-          </>
-        )}
-      </body>
-    </html>
-  )
-}
