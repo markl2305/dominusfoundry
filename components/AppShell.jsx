7883706de4830318efaeb9057d015df0b2c2e5a9
markl2305 <markflord@me.com>
2025-11-27 19:14:58 -0700

    Header

--- components/AppShell.jsx
+++ components/AppShell.jsx
@@ -1,11 +1,15 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
 import SiteHeader from './SiteHeader'
 
 export default function AppShell({ children }) {
+  const pathname = usePathname()
+  const hideHeader = pathname?.startsWith('/voice-concierge/campaign')
+
   return (
     <>
-      <SiteHeader />
+      {!hideHeader && <SiteHeader />}
       {children}
     </>
   )
