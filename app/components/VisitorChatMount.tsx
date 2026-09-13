96409f3511e3b1ee8a8c09eafeb87e3ddc6deafd
markl2305 <markflord@me.com>
2026-09-13 00:52:07 -0600

    Make Sabina orb heartbeat visibly pulse from within

--- app/components/VisitorChatMount.tsx
+++ app/components/VisitorChatMount.tsx
@@ -8,5 +8,5 @@ export default function VisitorChatMount(){
  const pathname=usePathname();
  const suppressed=/^\/(interview|evaluate|talk-to-iris)(\/|$)/.test(pathname||'');
  useEffect(()=>{wanted=!suppressed;signal(wanted?'mount':'unmount');return()=>{wanted=false;signal('unmount');};},[suppressed]);
- return <Script data-managed="true" id="sabina-visitor-chat" src="/sabina-chat/sabina-chat.js?v=3" strategy="afterInteractive" onReady={()=>{if(wanted)signal('mount');}}/>;
+ return <Script data-managed="true" id="sabina-visitor-chat" src="/sabina-chat/sabina-chat.js?v=4" strategy="afterInteractive" onReady={()=>{if(wanted)signal('mount');}}/>;
 }
